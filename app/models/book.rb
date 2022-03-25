d5f28639633cb89bee9f5a0ff11d81dc39a7ccef
Kuniaki Igarashi <igaiga@gmail.com>
2022-03-25 11:15:35 +0900

    Try inverse_of

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  #belongs_to :author, foreign_key: "user_id", class_name: "User"
+  #belongs_to :author, foreign_key: "user_id", class_name: "User", inverse_of: :books
+
   scope :ruby, ->{ where(title: "Ruby") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :books 
+  has_many :books
+  #has_many :books, inverse_of: :author
 end
